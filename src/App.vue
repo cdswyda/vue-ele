a0dbd8bd60d20cc9df0255a218c2ea0d6d7ad5c2
cdswyda <cdswyda@hotmail.com>
2017-08-31 20:14:07 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,57 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+<div id="app">
+  <v-header :seller="seller"></v-header>
+  <nav class="nav">
+    <div class="nav-item">商品</div>
+    <div class="nav-item">评论</div>
+    <div class="nav-item">商家</div>
+  </nav>
+  <div class="content"></div>
+  <img src="./assets/logo.png">
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
+import Header from './components/header/header'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      seller: {}
+    }
+  },
+  created () {
+    // let me = this
+    this.$http.get('/api/seller').then((response) => {
+      response = response.body
+      if (response.errno === 0) {
+        this.seller = response.data
+      }
+    })
+  },
+  components: {
+    'v-header': Header
+  }
 }
 </script>
 
-<style>
+<style lang="stylus">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
+.nav
+  display: flex
+  width: 100%
+  height: 40px
+  line-height: 40px
+  text-align: center
+  .nav-item
+    flex: 1
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,235 @@
+<template>
+  <header class="header">
+    <!-- 内容区域 -->
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img width="64" height="64" :src="seller.avatar" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}} / {{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon icon-keyboard_arrow_right"></i>
+      </div>
+      <div class="background">
+        <img :src="seller.avatar" width="100%" height="100%" alt="">
+      </div>
+    </div>
+    <!-- 公告区域 -->
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon icon-keyboard_arrow_right"></i>      
+    </div>
+    <!-- detail -->
+    <div class="detail" v-show="detailShow">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+          {{seller.bulletin}}
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon icon-close" @click="hideDetail"></i>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        detailShow: false
+      }
+    },
+    methods: {
+      showDetail () {
+        this.detailShow = true
+      },
+      hideDetail () {
+        this.detailShow = false
+      }
+    },
+    created () {
+      this.classMap = [
+        'decrease',
+        'discount',
+        'guarantee',
+        'invoice',
+        'special'
+      ]
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .header 
+    color: #fff
+    background: rgba(7,17,27,0.5)
+    position: relative
+    overflow: hidden
+    .content-wrapper
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: 0
+      .avatar
+        display: inline-block
+        vertical-align: top
+        img
+          border-radius: 2px
+      .content
+        font-size: 14px
+        display: inline-block
+        margin-left: 16px
+
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            width: 30px
+            height: 18px
+            bg-image('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+            vertical-align: top
+          .name 
+            margin-left: 6px
+            font-size: 16px
+            line-height: 18px
+            font-weight: bold
+        .description
+          font-size: 12px
+          margin-bottom: 10px
+          line-height: 12px
+        .support
+          font-size: 0
+          .icon
+            display: inline-block
+            vertical-align: top
+            width:12px
+            height:12px
+            margin-right: 4px;
+            background-size: 12px 12px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height: 12px
+            font-size: 10px
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 14px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0, 0, 0, .2)
+        text-align: center
+        .count          
+          font-size: 10px
+          vertical-align: top   
+          line-height: 24px;
+          display: inline-block;       
+        .icon
+          margin-left: 2px
+          font-size: 10px
+          line-height: 24px        
+
+    .bulletin-wrapper 
+      position: relative
+      height: 28px
+      padding: 0 22px 0 12px
+      line-height: 0
+      font-size: 10px
+      text-overflow: ellipsis
+      white-space: nowrap
+      overflow: hidden
+      background: rgba(7,17,27,0.2)
+      .bulletin-title
+        display: inline-block
+        vertical-align: top
+        margin-top: 8px
+        width: 22px
+        height: 12px
+        bg-image('bulletin')
+        background-size: 22px 12px
+        background-repeat: no-repeat
+      .bulletin-text        
+        margin-left: 4px
+        line-height: 28px
+      .icon
+        display: inline-block
+        position: absolute;
+        right: 12px
+        top: 9px
+    .background
+      position: absolute
+      z-index: -1
+      top: 0
+      right: 0
+      bottom: 0
+      left: 0
+      filter: blur(10px)
+    .detail 
+      position: fixed;
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background: rgba(7,17,27,.8)
+      
+      .detail-wrapper 
+        min-height: 100%
+        overflow: auto
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+
+      .detail-close
+        position: relative
+        width: 32px
+        height: 32px
+        margin: -64px auto 0
+        clear: both
+        .icon
+          font-size: 34px
+</style>
